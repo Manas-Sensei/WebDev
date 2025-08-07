9e24cc41cebcd69b266beb44a92625661816bb9a
Manas Ranjan Sahu <manassensei@gmail.com>
2025-08-07 06:27:33 +0530

    Delete Practice.py

--- Practice.py
+++
@@ -1,4 +0,0 @@
-user_name = input("Enter your name: ")
-user_age = int(input("Enter your age: "))
-fav_color = input("Enter your favorite color: ")    
-print("Hello "+ user_name + "! you are "+ str(user_age) + " years old and your favorite color is " + fav_color + ".")
