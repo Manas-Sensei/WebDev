2cbea72283cc4c97205c9f4d4abe49a498017103
Manas Ranjan Sahu <manassensei@gmail.com>
2025-08-07 11:13:47 +0530

    Create Personal info.py

---
+++ Python Programs/Personal info.py
@@ -0,0 +1,11 @@
+name = "Manas"
+age = 23
+country = "India"
+favorite_color = "Green"
+
+print("***********************")
+print("*  Name:", name, " " * (12-len(name)), "*")
+print("*  Age:", age, " " * (14-len(str(age))), "*")
+print("*  Country:", country, " " * (10-len(country)), "*")
+print("*  Favorite Color:", favorite_color, " " * (6-len(favorite_color)), "*")
+print("***********************")
