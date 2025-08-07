5c2a8bca23e28150f0960c5438e4a0720e472fb2
Manas Ranjan Sahu <manassensei@gmail.com>
2025-08-07 06:26:20 +0530

    Create Practice.py

---
+++ Practice.py
@@ -0,0 +1,4 @@
+user_name = input("Enter your name: ")
+user_age = int(input("Enter your age: "))
+fav_color = input("Enter your favorite color: ")    
+print("Hello "+ user_name + "! you are "+ str(user_age) + " years old and your favorite color is " + fav_color + ".")
